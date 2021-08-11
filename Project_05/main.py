5d1cb242d46dce7bddc1ae67e3393373d31776f0
Ramon Valeriano <rrvaleriano@gmail.com>
2021-08-11 17:07:50 -0300

    11082021_4-atualizando-as-funcionalidades

--- Project_05/main.py
+++ Project_05/main.py
@@ -9,7 +9,7 @@ from typing import List
 app = FastAPI()
 
 class Animal(BaseModel):
-    id : Optional[int]
+    id : Optional[str]
     nome : str
     idade : int
     sexo : str
@@ -22,9 +22,34 @@ def lista_animais():
 
     return banco
 
+
+@app.get('/animais/{animdal_id}')
+def obter_animal(animdal_id: str):
+    for animal in banco:
+        if animal.id == animdal_id:
+            return animal
+
+    return {'mensagem': 'Animal não localizado'}
+
+
 @app.post('/animais')
 def criar_animel(animal: Animal):
-    animal.id = uuid4()
+    animal.id = str(uuid4())
     banco.append(animal)
 
     return {'mensagem':'Animal Adicionado'}
+
+
+@app.delete('/animais/{animal_id}')
+def removendo_animal(animal_id: str):
+    posicao = None
+    for index, animal in enumerate(banco):
+        if animal.id == animal_id:
+            posicao = index
+            break
+    
+    if posicao:
+        animal_removido = banco.pop(posicao)
+        return {'mensagem':animal_removido}
+
+    return {'mensagem': 'Animal não foi encontrado.'}
