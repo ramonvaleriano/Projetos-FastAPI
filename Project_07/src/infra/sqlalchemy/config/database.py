e18066530a9d68432e970bfacd96b0bd7c9951f6
Ramon Valeriano <rrvaleriano@gmail.com>
2021-08-12 16:08:47 -0300

    12082021_7-criando-primeiro-esquema-de-banco-de-dados

---
+++ Project_07/src/infra/sqlalchemy/config/database.py
@@ -0,0 +1,32 @@
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app_pro07.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def criar_banco_de_dados():
+    """
+        Função para criar o banco de dados
+    """
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    """
+        Função para obter o bando de dados
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
