9166916472ff79699eaf680550526071ee750e50
Ramon Valeriano <rrvaleriano@gmail.com>
2021-08-12 20:32:26 -0300

    12082021_8-criando-estrutura-inicial-banco-de-dados

---
+++ Project_08/src/infra/sqlalchemy/models/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+
+from infra.sqlalchemy.config.database import Base
+
+class Serie(Base):
+
+    __tablename__ = 'serie'
+
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String)
+    ano = Column(Integer)
+    genero = Column(String)
+    qtd_temporadas = Column(Integer)
