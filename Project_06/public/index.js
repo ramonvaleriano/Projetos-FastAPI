d8d171ebda907eee72dd25538d45a220bdf1534d
Ramon Valeriano <rrvaleriano@gmail.com>
2021-08-12 10:21:01 -0300

    12082021_3-backend-enviado-dados-para-o-front

--- Project_06/public/index.js
+++ Project_06/public/index.js
@@ -1,6 +1,18 @@
-function carregarAnimais(){
-    axios.get('http://localhost:8000/animais')
-    .then(response => console.log(response.data))
+async function carregarAnimais(){
+    const response = await axios.get('http://localhost:8000/animais')
+    
+    const animais = response.data
+
+    const lista = document.getElementById('lista-animais')
+
+    animais.forEach(animal => {
+        const item = document.createElement('li')
+        item.innerText = animal.nome
+
+        lista.appendChild(item)
+    });
+
+    
 }
 
 function app(){
